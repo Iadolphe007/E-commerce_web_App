dede1bb588efed3a6b63f3761d8d2412091f14c0
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-03-31 07:42:56 +0000

    Footer modification

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -24,7 +24,9 @@ const Footer = () => {
             <button>Subscribe</button>
         </div>
       </div>
+      <p>Developers</p>
       <div className="footer-social-icon">
+      <p>Imanishimwe Adolphe</p>
         <div className="footer-icon-container">
             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
         </div>
@@ -32,13 +34,31 @@ const Footer = () => {
             <a href="https://twitter.com/iadolphe007" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
         </div>
         <div className="footer-icon-container">
-            <a href="https://google.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+            <a href="mailto:example@example.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
         </div>
         <div className="footer-icon-container">
             <a href="https://www.linkedin.com/in/imanishimwe-adolphe-b14623250/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
         </div>
         <div className="footer-icon-container">
             <a href="https://github.com/Iadolphe007" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+        </div>
+      </div>
+      <div className="footer-social-icon">
+        <p>Mutaganda Elysee</p>
+        <div className="footer-icon-container">
+            <a href="https://www.instagram.com/mutagandaelysee?igsh=eW90d2dnMHR5djF1" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+        </div>
+        <div className="footer-icon-container">
+            <a href="https://x.com/MutagandaElysee?t=f34YZEq-6EJCn0alFEmwag&s=09" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+        </div>
+        <div className="footer-icon-container">
+            <a href="mailto:mutagandaelysee2@gmail.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+        </div>
+        <div className="footer-icon-container">
+            <a href="https://www.linkedin.com/in/mutagandaelysee2/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
+        </div>
+        <div className="footer-icon-container">
+            <a href="https://github.com/M-Elysee" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
         </div>
       </div>
       <div className="footer-copyright">
