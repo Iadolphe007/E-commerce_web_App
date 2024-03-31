d983f051aac3be2f0b02b9d8050ba89c7f9db75f
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-03-31 08:28:47 +0000

    contact page and footer modification

--- frontend/src/components/ContactPage/ContactPage.jsx
+++ frontend/src/components/ContactPage/ContactPage.jsx
@@ -11,23 +11,53 @@ const ContactPage = () => {
   return (
     <div className='contactPage'>
         <img src={contact_banner} alt="" />
-        <div className="socials">
-            <div className="footer-icon-container">
-                <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+        <h1>Meet the Developers</h1>
+        <p>Hello there! 👋 We're Imanishimwe Adolphe and Mutaganda Elysee, a dynamic duo of full-stack developers passionate about crafting exceptional digital experiences.</p>
+        <div className='developer'>
+            <h2>About Imanishimwe Adolphe</h2>
+            <p>I'm Imanishimwe Adolphe, a dedicated full-stack developer with a keen eye for frontend design and a knack for backend architecture. With a strong foundation in JavaScript, HTML, and CSS, I specialize in bringing creative ideas to life through intuitive user interfaces and robust backend systems.</p>
+            <div className="socials">
+                <div className="footer-icon-container">
+                    <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://google.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://github.com/" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+                </div>
             </div>
-            <div className="footer-icon-container">
-                <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+        </div>
+        <div className='developer'>
+            <h2>About Mutaganda Elysee</h2>
+            <p>I'm Mutaganda Elysee, a talented full-stack developer with expertise in frontend development and backend integration. With a passion for problem-solving and a meticulous attention to detail, I brought invaluable skills to our projects, ensuring they meet the highest standards of functionality and performance.</p>
+            <div className="socials">
+                <div className="footer-icon-container">
+                    <a href="https://www.instagram.com/mutagandaelysee?igsh=eW90d2dnMHR5djF1" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://x.com/MutagandaElysee?t=f34YZEq-6EJCn0alFEmwag&s=09" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="mailto:mutagandaelysee2@gmail.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://www.linkedin.com/in/mutagandaelysee2/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
+                </div>
+                <div className="footer-icon-container">
+                    <a href="https://github.com/M-Elysee" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+                </div>
             </div>
-            <div className="footer-icon-container">
-                <a href="https://google.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
-            </div>
-            <div className="footer-icon-container">
-                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
-            </div>
-            <div className="footer-icon-container">
-                <a href="https://github.com/" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
-            </div>
-            </div>
+        </div>
+        <h2>Our Collaboration</h2>
+        <p>Together, we collaborated on this ecommerce portfolio project, leveraging our combined skills and expertise to create a seamless and visually stunning online shopping experience. we worked hand in hand to deliver a product that not only showcases our technical abilities but also reflects our dedication to excellence.</p>
+        <p>We're excited to share our project with you and invite you to explore the innovative features and meticulous craftsmanship that went into its creation. Thank you for visiting, and feel free to reach out if you have any questions or feedback!</p>
         <div>
             <h4>get in touch with our socials or contact via email</h4>
         </div>
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -25,40 +25,44 @@ const Footer = () => {
         </div>
       </div>
       <p>Developers</p>
-      <div className="footer-social-icon">
-      <p>Imanishimwe Adolphe</p>
-        <div className="footer-icon-container">
-            <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://twitter.com/iadolphe007" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="mailto:example@example.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://www.linkedin.com/in/imanishimwe-adolphe-b14623250/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://github.com/Iadolphe007" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+      <div>
+        <p>Imanishimwe Adolphe</p>
+        <div className="footer-social-icon">
+          <div className="footer-icon-container">
+              <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://twitter.com/iadolphe007" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="mailto:imanishimwe@gmail.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://www.linkedin.com/in/imanishimwe-adolphe-b14623250/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://github.com/Iadolphe007" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+          </div>
         </div>
       </div>
-      <div className="footer-social-icon">
+      <div>
         <p>Mutaganda Elysee</p>
-        <div className="footer-icon-container">
-            <a href="https://www.instagram.com/mutagandaelysee?igsh=eW90d2dnMHR5djF1" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://x.com/MutagandaElysee?t=f34YZEq-6EJCn0alFEmwag&s=09" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="mailto:mutagandaelysee2@gmail.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://www.linkedin.com/in/mutagandaelysee2/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
-        </div>
-        <div className="footer-icon-container">
-            <a href="https://github.com/M-Elysee" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+        <div className="footer-social-icon">
+          <div className="footer-icon-container">
+              <a href="https://www.instagram.com/mutagandaelysee?igsh=eW90d2dnMHR5djF1" target="_blank" rel="noreferrer"><img src={instagram_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://x.com/MutagandaElysee?t=f34YZEq-6EJCn0alFEmwag&s=09" target="_blank" rel="noreferrer"><img src={twitter_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="mailto:mutagandaelysee2@gmail.com" target="_blank" rel="noreferrer"><img src={email_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://www.linkedin.com/in/mutagandaelysee2/" target="_blank" rel="noreferrer"><img src={linked_icon} alt="" /></a>
+          </div>
+          <div className="footer-icon-container">
+              <a href="https://github.com/M-Elysee" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+          </div>
         </div>
       </div>
       <div className="footer-copyright">
